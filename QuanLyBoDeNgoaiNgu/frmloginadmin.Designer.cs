27179040f47403360aded0777900d4f0fb66b170
Chương Đoàn <chuongdoan2102@gmail.com>
2023-04-26 21:19:13 +0700

    cãi con cặc

--- QuanLyBoDeNgoaiNgu/frmloginadmin.Designer.cs
+++ QuanLyBoDeNgoaiNgu/frmloginadmin.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lblMaDangNhap = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMaDangNhap = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,15 +56,40 @@
             this.panel1.Size = new System.Drawing.Size(426, 265);
             this.panel1.TabIndex = 0;
             // 
-            // lblMaDangNhap
+            // button2
             // 
-            this.lblMaDangNhap.AutoSize = true;
-            this.lblMaDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMaDangNhap.Location = new System.Drawing.Point(29, 29);
-            this.lblMaDangNhap.Name = "lblMaDangNhap";
-            this.lblMaDangNhap.Size = new System.Drawing.Size(110, 26);
-            this.lblMaDangNhap.TabIndex = 0;
-            this.lblMaDangNhap.Text = "Tài Khoản";
+            this.button2.Location = new System.Drawing.Point(239, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 40);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(34, 141);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(347, 41);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 58);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(347, 41);
+            this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
@@ -77,39 +102,15 @@
             this.label1.Text = "Mật Khẩu";
             this.label1.Click += new System.EventHandler(this.lblMaDangNhap_Click);
             // 
-            // textBox1
+            // lblMaDangNhap
             // 
-            this.textBox1.Location = new System.Drawing.Point(34, 58);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(347, 41);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(34, 141);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(347, 41);
-            this.textBox2.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(34, 198);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(142, 40);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(239, 198);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(142, 40);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Cancel";
-            this.button2.UseVisualStyleBackColor = true;
+            this.lblMaDangNhap.AutoSize = true;
+            this.lblMaDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMaDangNhap.Location = new System.Drawing.Point(29, 29);
+            this.lblMaDangNhap.Name = "lblMaDangNhap";
+            this.lblMaDangNhap.Size = new System.Drawing.Size(110, 26);
+            this.lblMaDangNhap.TabIndex = 0;
+            this.lblMaDangNhap.Text = "Tài Khoản";
             // 
             // frmloginadmin
             // 
