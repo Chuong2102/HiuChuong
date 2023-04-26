8f21ec3eafa9ecb38592048afc87bef1bdb21b66
CanhHieuMU <20T1080063@husc.edu.vn>
2023-04-26 23:28:10 +0700

    thanks

--- QuanLyBoDeNgoaiNgu/frmloginstudent.cs
+++ QuanLyBoDeNgoaiNgu/frmloginstudent.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using QuanLyBoDeNgoaiNgu.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity.Infrastructure;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,14 +14,30 @@ namespace QuanLyBoDeNgoaiNgu
 {
     public partial class frmloginstudent : Form
     {
+        QuanLyBoDeNgoaiNguModel1 model;
         public frmloginstudent()
         {
+            model = new QuanLyBoDeNgoaiNguModel1();
+
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (model.Accounts.Select(a => a.Username == textBox1.Text && a.Password == textBox2.Text).FirstOrDefault())
+            {
+                MessageBox.Show("Đăng nhập thành công!");
+            }
+            else
+            {
+                MessageBox.Show("Đăng nhập không thành công!");
+            }
+        }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            
         }
     }
 }
