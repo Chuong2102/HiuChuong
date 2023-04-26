4af4c55b4f9248ab85e5cf067dbf7f4cd09e4912
Chương Đoàn <chuongdoan2102@gmail.com>
2023-04-26 23:30:12 +0700

    ars 7 -0 man cute

--- QuanLyBoDeNgoaiNgu/frmloginadmin.cs
+++ QuanLyBoDeNgoaiNgu/frmloginadmin.cs
@@ -7,24 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QuanLyBoDeNgoaiNgu.Infrastructure;
+using QuanLyBoDeNgoaiNgu.Entities;
+using System.Data.Entity;
 
 namespace QuanLyBoDeNgoaiNgu
 {
     public partial class frmloginadmin : Form
     {
+        QuanLyBoDeNgoaiNguModel1 model;
+
         public frmloginadmin()
         {
+            model = new QuanLyBoDeNgoaiNguModel1();
             InitializeComponent();
         }
 
         private void lblMaDangNhap_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if(model.Accounts.Select(c => c.Username == textBox1.Text && c.Password == textBox2.Text).FirstOrDefault() == true)
+            {
+                MessageBox.Show("Dang nhap thanh cong");
+            }
+            else
+            {
+                MessageBox.Show("Dang nhap k thanh cong");
+            }
 
+            
         }
     }
 }
