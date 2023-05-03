9bb7df7bf3885d4ba63e9d53b164a4eaab04840c
Chương Đoàn <chuongdoan2102@gmail.com>
2023-05-04 00:22:07 +0700

    cc

---
+++ QuanLyBoDeNgoaiNgu/Entities/GroupQuestion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBoDeNgoaiNgu.Entities
+{
+    public class GroupQuestion
+    {
+        [Key]
+        public int GroupQuestionID { get; set; }
+        public string GroupQuestionName { get; set; }
+        
+    }
+}
