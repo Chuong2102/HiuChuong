723c46f45fa6f109358b19d1ea7e0700380f0b65
CanhHieuMU <20T1080063@husc.edu.vn>
2023-05-05 09:43:51 +0700

    ahihi

--- QuanLyBoDeNgoaiNgu/ThemBoDapAn.Designer.cs
+++ QuanLyBoDeNgoaiNgu/ThemBoDapAn.Designer.cs
@@ -38,6 +38,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -110,7 +111,7 @@
             this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
             this.button1.Location = new System.Drawing.Point(31, 401);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(275, 40);
+            this.button1.Size = new System.Drawing.Size(190, 40);
             this.button1.TabIndex = 16;
             this.button1.Text = "ADD";
             this.button1.UseVisualStyleBackColor = false;
@@ -119,11 +120,11 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.button2.Location = new System.Drawing.Point(322, 401);
+            this.button2.Location = new System.Drawing.Point(410, 401);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(281, 40);
+            this.button2.Size = new System.Drawing.Size(193, 40);
             this.button2.TabIndex = 16;
-            this.button2.Text = "Cancel";
+            this.button2.Text = "CANCEL";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -137,12 +138,24 @@
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button3.Location = new System.Drawing.Point(227, 401);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 40);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "DELETE";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
             // ThemBoDapAn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnTiengHan);
@@ -171,5 +184,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- QuanLyBoDeNgoaiNgu/ThemSuatThi.Designer.cs
+++ QuanLyBoDeNgoaiNgu/ThemSuatThi.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cmbBacST = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // button1
@@ -49,30 +49,21 @@
             this.button1.Text = "ADD";
             this.button1.UseVisualStyleBackColor = false;
             // 
-            // comboBox1
+            // cmbBacST
             // 
-            this.comboBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.cmbBacST.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.cmbBacST.FormattingEnabled = true;
+            this.cmbBacST.Items.AddRange(new object[] {
             "A1",
             "A2",
             "B1",
             "B2",
             "C1",
             "C2"});
-            this.comboBox1.Location = new System.Drawing.Point(565, 196);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(107, 24);
-            this.comboBox1.TabIndex = 9;
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.textBox2.Location = new System.Drawing.Point(198, 187);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(327, 42);
-            this.textBox2.TabIndex = 7;
+            this.cmbBacST.Location = new System.Drawing.Point(565, 196);
+            this.cmbBacST.Name = "cmbBacST";
+            this.cmbBacST.Size = new System.Drawing.Size(107, 24);
+            this.cmbBacST.TabIndex = 9;
             // 
             // label3
             // 
@@ -94,15 +85,6 @@
             this.label2.TabIndex = 5;
             this.label2.Text = "Thời gian bắt đầu";
             // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.textBox1.Location = new System.Drawing.Point(198, 98);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(327, 42);
-            this.textBox1.TabIndex = 8;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -123,14 +105,33 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "Thời gian kết thúc";
             // 
-            // textBox3
+            // dateTimePicker1
             // 
-            this.textBox3.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.textBox3.Location = new System.Drawing.Point(198, 276);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(327, 42);
-            this.textBox3.TabIndex = 7;
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(201, 110);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(339, 30);
+            this.dateTimePicker1.TabIndex = 11;
+            this.dateTimePicker1.Value = new System.DateTime(2023, 5, 5, 9, 33, 59, 0);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker2.Location = new System.Drawing.Point(241, 196);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(252, 30);
+            this.dateTimePicker2.TabIndex = 12;
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker3.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.dateTimePicker3.Location = new System.Drawing.Point(241, 286);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(252, 30);
+            this.dateTimePicker3.TabIndex = 20;
             // 
             // ThemSuatThi
             // 
@@ -138,14 +139,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.cmbBacST);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "ThemSuatThi";
             this.Text = "ThemSuatThi";
@@ -157,13 +158,13 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox cmbBacST;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
     }
 }
