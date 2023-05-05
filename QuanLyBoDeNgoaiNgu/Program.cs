bbe534d151ce6ef091c6d16e918b3a5bc12c3aaf
Chương Đoàn <chuongdoan2102@gmail.com>
2023-05-05 09:44:11 +0700

    nhu buoi

--- QuanLyBoDeNgoaiNgu/Program.cs
+++ QuanLyBoDeNgoaiNgu/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyBoDeNgoaiNgu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ThemCauHoi());
+            Application.Run(new frmloginstudent());
         }
     }
 }
