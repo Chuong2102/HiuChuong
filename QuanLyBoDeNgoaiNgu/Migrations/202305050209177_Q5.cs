ebc4d161339ce04c1074e72de30ed246dd35289d
Chương Đoàn <chuongdoan2102@gmail.com>
2023-05-05 09:11:40 +0700

    gui bro iu dau

---
+++ QuanLyBoDeNgoaiNgu/Migrations/202305050209177_Q5.cs
@@ -0,0 +1,33 @@
+﻿namespace QuanLyBoDeNgoaiNgu.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Q5 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.GroupQuestions",
+                c => new
+                    {
+                        GroupQuestionID = c.Int(nullable: false, identity: true),
+                        GroupQuestionName = c.String(),
+                        Question_QuestionID = c.Int(),
+                    })
+                .PrimaryKey(t => t.GroupQuestionID)
+                .ForeignKey("dbo.Questions", t => t.Question_QuestionID)
+                .Index(t => t.Question_QuestionID);
+            
+            AddColumn("dbo.Questions", "Text", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.GroupQuestions", "Question_QuestionID", "dbo.Questions");
+            DropIndex("dbo.GroupQuestions", new[] { "Question_QuestionID" });
+            DropColumn("dbo.Questions", "Text");
+            DropTable("dbo.GroupQuestions");
+        }
+    }
+}
