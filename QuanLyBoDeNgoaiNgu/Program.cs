617fb74835f44cebee2fd476935bec9f0b441cfd
Chương Đoàn <chuongdoan2102@gmail.com>
2023-05-05 10:49:52 +0700

    from Teo` with luv

--- QuanLyBoDeNgoaiNgu/Program.cs
+++ QuanLyBoDeNgoaiNgu/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyBoDeNgoaiNgu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ThemBoDapAn());
+            Application.Run(new trangkhivaostudent());
         }
     }
 }
