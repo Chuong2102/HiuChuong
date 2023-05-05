f935660e3bb0e443d4d83f0bf672d398b72f295b
CanhHieuMU <20T1080063@husc.edu.vn>
2023-05-05 09:17:03 +0700

    gui anh iu

---
+++ QuanLyBoDeNgoaiNgu/frmThiSinhVien.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyBoDeNgoaiNgu
+{
+    public partial class frmThiSinhVien : Form
+    {
+        public frmThiSinhVien()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmThiSinhVien_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
